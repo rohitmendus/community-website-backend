240392004306f66b138c70bbf35070acb652eafa
rohitmendus <rohitmendus@gmail.com>
2022-02-15 19:48:57 +0530

    Increased the security of the project

--- project/project/settings.py
+++ project/project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_g0!@gyv_k8bn_h-^+2g)ujhs)^#%j5ly336#*ix=lljr+*=2a'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -140,5 +141,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'junglesquad14@gmail.com'
-EMAIL_HOST_PASSWORD = '#programmingisfun14'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
--- project/website/models.py
+++ project/website/models.py
@@ -1,7 +1,46 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class friend_requests(models.Model):
 	from_user = models.ForeignKey(User, related_name='from_user', on_delete=models.CASCADE)
 	to_user = models.ForeignKey(User, related_name='to_user', on_delete=models.CASCADE)
+
+
+class Articles(models.Model):
+	title = models.CharField(max_length=100, null=True)
+	text = models.TextField(max_length=500, null=True)
+	author = models.ManyToManyField(User, related_name="articles", blank=True)
+	date_posted = models.DateField(auto_now=True)
+	like = models.IntegerField(null=True, default=0)
+
+class ArticleComments:
+	article = models.ManyToManyField(Articles, related_name="articles", blank=True)
+	comment = models.TextField(max_length=500, null=True)
+	like = models.IntegerField(null=True, default=0)
+	posted_by = models.ManyToManyField(User, related_name="article_comments", blank=True)
+	date_posted = models.DateField(auto_now=True)
+
+class Books(models.Model):
+	title = models.CharField(max_length=100, null=True)
+	posted_by = models.ManyToManyField(User, related_name="books", blank=True)
+	cover = models.ImageField(upload_to="pics", null=True)
+	date_posted = models.DateField(auto_now=True)
+
+class BookReview(models.Model):
+	book = models.ManyToManyField(Books, related_name="book_reviews", blank=True)
+	posted_by = models.ManyToManyField(User, related_name="book_review", blank=True)
+	review = models.TextField(max_length=500, null=True)
+	rating = models.FloatField(default=0, 
+		validators=[
+			MinValueValidator(0.0), 
+			MaxValueValidator(5.0)
+		])
+	date_posted = models.DateField(auto_now=True)
+
+class GalleryImages(models.Model):
+	image = models.ImageField(upload_to="pics", null=True)
+	caption = models.CharField(max_length=100, null=True)
+	posted_by = models.ManyToManyField(User, related_name="galley_images", blank=True)
+	like = models.ManyToManyField(User, related_name="images", blank=True)
